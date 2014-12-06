5ce2a6d667540d89af0fb0cbdb2ecb10e139e91e
lukehoffmann <lukehoffmann@outlook.com>
2014-12-06 15:13:50 +1030

    refactored problem 1 into LINQ

--- ProjectEulerInCSharp/Solutions.cs
+++ ProjectEulerInCSharp/Solutions.cs
@@ -8,22 +8,23 @@ namespace ProjectEulerInCSharp
 {
     public class Solutions
     {
+
+        /// <summary>
+        /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+        /// The sum of these multiples is 23.
+        /// Find the sum of all the multiples of 3 or 5 below the given limit.
+        /// </summary>
+        /// <param name="below"></param>
         public Int32 SumOfMultiplesOfThreeOrFive(Int32 below)
         {
-            //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
-            //The sum of these multiples is 23.
-            //Find the sum of all the multiples of 3 or 5 below 1000.
-            if (below < 1) { throw new ArgumentOutOfRangeException(); };
-       
-            Int32 result = 0;
-            for (Int32 i = 1; i < below; i++)
+            if (below < 1)
             {
-                if (i % 3 == 0 || i % 5 == 0)
-                {
-                    result += i;
-                }
-            }
-            return result;
+                throw new ArgumentOutOfRangeException();
+            };
+
+            return Enumerable.Range(1, below - 1)
+                .Where(i => (i % 3 == 0 || i % 5 == 0))
+                .Sum();
         }
     }
 }
