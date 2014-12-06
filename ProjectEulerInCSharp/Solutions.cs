a25ed6ba1f5673f03c888921a23f99b12681a7c0
lukehoffmann <lukehoffmann@outlook.com>
2014-12-06 15:02:07 +1030

    solved problem 1

--- ProjectEulerInCSharp/Solutions.cs
+++ ProjectEulerInCSharp/Solutions.cs
@@ -8,12 +8,22 @@ namespace ProjectEulerInCSharp
 {
     public class Solutions
     {
-        public Int32 problem1
+        public Int32 SumOfMultiplesOfThreeOrFive(Int32 below)
         {
             //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
             //The sum of these multiples is 23.
             //Find the sum of all the multiples of 3 or 5 below 1000.
-
+            if (below < 1) { throw new ArgumentOutOfRangeException(); };
+       
+            Int32 result = 0;
+            for (Int32 i = 1; i < below; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+            return result;
         }
     }
 }
---
+++ ProjectEulerTests/UnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEulerInCSharp;
+
+namespace ProjectEulerTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Problem1TestControl()
+        {
+            //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+            //The sum of these multiples is 23.
+            Solutions euler = new Solutions();
+            Int32 result = euler.SumOfMultiplesOfThreeOrFive(10);
+
+            Assert.AreEqual(23, result);
+        }
+
+        [TestMethod]
+        public void Problem1Test()
+        {
+            //Find the sum of all the multiples of 3 or 5 below 1000.
+            Solutions euler = new Solutions();
+            Int32 result = euler.SumOfMultiplesOfThreeOrFive(1000);
+            System.Diagnostics.Debug.WriteLine(result);
+            Assert.AreEqual(233168, result);
+        }
+
+    }
+}
