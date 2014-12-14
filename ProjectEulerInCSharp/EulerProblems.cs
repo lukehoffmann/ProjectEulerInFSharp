46eb733ac1bd1b8cfca4d80c7b5690ad4f032bef
lukehoffmann <lukehoffmann@outlook.com>
2014-12-14 15:20:56 +1030

    Refactor

---
+++ ProjectEulerInCSharp/EulerProblems.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerInCSharp
+{
+    public class EulerProblems
+    {
+        /// <summary>
+        /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+        /// The sum of these multiples is 23.
+        /// </summary>
+        public static int EulerProblem1Example()
+        {
+            //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+            //The sum of these multiples is 23.
+            return Enumerable.Range(0, 10)
+                .Where(i => (i.MultipleOf(3) || i.MultipleOf(5)))
+                .Sum();
+        }
+
+        /// <summary>
+        /// Find the sum of all the multiples of 3 or 5 below 1000.
+        /// </summary>
+        public static int EulerProblem1()
+        {
+            //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+            //The sum of these multiples is 23.
+            return Enumerable.Range(0, 1000)
+                .Where(i => (i.MultipleOf(3) || i.MultipleOf(5)))
+                .Sum();
+        }
+
+        /// <summary>
+        /// By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+        /// </summary>
+        /// <returns></returns>
+        public static int EulerProblem2()
+        {
+            return MathHelpers.FibonacciNumbersLessThan(4000000)
+                    .Where(f => f.MultipleOf(2))
+                    .Sum();
+        }
+    }
+}
--- ProjectEulerTests/EulerProblemTests.cs
+++ ProjectEulerTests/EulerProblemTests.cs
@@ -8,40 +8,38 @@ namespace ProjectEulerTests
     [TestClass]
     public class EulerProblemTests
     {
+
         [TestMethod]
-        public void EulerProblem1Test1()
+        public void EulerProblem1ExampleTest()
         {
             //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
             //The sum of these multiples is 23.
-            MathHelpers euler = new MathHelpers();
-            Int32 result = euler.SumOfMultiplesOfThreeOrFive(10);
+            Int32 result = EulerProblems.EulerProblem1Example();
 
             Assert.AreEqual(23, result);
         }
 
         [TestMethod]
-        public void EulerProblem1Solution()
+        public void EulerProblem1SolutionTest()
         {
             //Find the sum of all the multiples of 3 or 5 below 1000.
-            MathHelpers euler = new MathHelpers();
             Stopwatch watch = Stopwatch.StartNew();
-            Int32 result = euler.SumOfMultiplesOfThreeOrFive(1000);
+            Int32 result = EulerProblems.EulerProblem1();
 
             watch.Stop();
 
             Console.WriteLine(String.Format("Problem {0} = {1}  ({2}ms)", 1, result, watch.ElapsedMilliseconds));
-           
+
             Assert.AreEqual(233168, result);
         }
 
         [TestMethod]
         public void EulerProblem2Solution()
         {
-            MathHelpers euler = new MathHelpers();
-           //By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+            //By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
             Stopwatch watch = Stopwatch.StartNew();
-            Int32 result = euler.SumOfEvenFibonacciNumbers(4000000);
-          
+            Int32 result = EulerProblems.EulerProblem2();
+
             watch.Stop();
             Console.WriteLine(String.Format("Problem {0} = {1}  ({2}ms)", 2, result, watch.ElapsedMilliseconds));
 
