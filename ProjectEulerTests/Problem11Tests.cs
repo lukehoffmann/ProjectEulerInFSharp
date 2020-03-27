04c6f51a5152ca118f04693d51dbe9ee660d7e98
Luke Hoffmann <luke.hoffmann@optimatics.com>
2020-03-27 19:20:37 +1030

    Attempts to solve Problem 11
    The answer is wrong

---
+++ ProjectEulerTests/Problem11Tests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEulerInCSharp;
+
+namespace ProjectEulerTests
+{
+    [TestClass]
+    public class Problem11Tests
+    {
+        [TestMethod]
+        public void RightProductTest()
+        {
+            Assert.AreEqual(08 * 02 * 22 * 97, Problem11.RightProduct(1, 1));
+        }
+
+        [TestMethod]
+        public void DownProductTest()
+        {
+            Assert.AreEqual(08 * 49 * 81 * 52, Problem11.DownProduct(1, 1));
+        }
+
+        [TestMethod]
+        public void RightDownProductTest()
+        {
+            Assert.AreEqual(08 * 49 * 31 * 23, Problem11.RightDownProduct(1, 1));
+        }
+
+        [TestMethod]
+        public void LeftDownProductTest()
+        {
+            Assert.AreEqual(97 * 99 * 49 * 52, Problem11.LeftDownProduct(4, 1));
+        }
+    }
+}
