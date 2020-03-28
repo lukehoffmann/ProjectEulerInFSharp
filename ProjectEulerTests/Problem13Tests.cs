66000353edd1c096a45f7b48b7a243e28624c10e
Luke Hoffmann <luke.hoffmann@optimatics.com>
2020-03-28 14:49:26 +1030

    Start of Problem 13
    Naive implementation of adding numbers as strings

---
+++ ProjectEulerTests/Problem13Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEulerInCSharp;
+
+namespace ProjectEulerTests
+{
+    [TestClass]
+    public class Problem13Tests
+    {
+        [TestMethod]
+        public void AddTwoNumbersAsStrings_NoRemainders()
+        {
+            Assert.AreEqual("5432", Problem13.AddNumbersAsStrings("1111", "4321"));
+            Assert.AreEqual("5555", Problem13.AddNumbersAsStrings("1234", "4321"));
+            Assert.AreEqual("3348", Problem13.AddNumbersAsStrings("1111", "1234", "1000", "3"));
+        }
+    }
+}
